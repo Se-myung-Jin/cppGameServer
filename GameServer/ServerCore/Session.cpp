5594e5a6241050f73bac2bdaf4cfb1ee966b218e
Semyung Jin <tpaud94@naver.com>
2025-12-05 14:09:40 +0900

    [ServerCore] Send Lock 개선

--- GameServer/ServerCore/Session.cpp
+++ GameServer/ServerCore/Session.cpp
@@ -15,18 +15,21 @@ Session::~Session()
 
 void Session::Send(SendBufferRef sendBuffer)
 {
-	// 현재 RegisterSend가 걸리지 않은 상태라면, 걸어준다
-	WRITE_LOCK;
+	if (IsConnected() == false)
+		return;
 
-	_sendQueue.push(sendBuffer);
+	bool registerSend = false;
 
-	/*if (_sendRegistered == false)
 	{
-		_sendRegistered = true;
-		RegisterSend();
-	}*/
+		WRITE_LOCK;
 
-	if (_sendRegistered.exchange(true) == false)
+		_sendQueue.push(sendBuffer);
+
+		if (_sendRegistered.exchange(true) == false)
+			registerSend = true;
+	}
+
+	if (registerSend)
 		RegisterSend();
 }
 

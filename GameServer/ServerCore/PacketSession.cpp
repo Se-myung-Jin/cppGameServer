a8381df4efcf476df1df96fae8f31cd7b1af3dbc
Semyung Jin <tpaud94@naver.com>
2025-12-05 14:19:31 +0900

    [ServerCore] PacketSession 1차 추가

---
+++ GameServer/ServerCore/PacketSession.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "PacketSession.h"
+
+PacketSession::PacketSession()
+{
+}
+
+PacketSession::~PacketSession()
+{
+}
+
+int32 PacketSession::OnRecv(BYTE* buffer, int32 len)
+{
+	return int32();
+}
---
+++ GameServer/ServerCore/PacketSession.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Session.h"
+
+
+struct PacketHeader
+{
+	uint16 size;
+	uint16 id;
+};
+
+
+class PacketSession : public Session
+{
+public:
+	PacketSession();
+	virtual ~PacketSession();
+
+	PacketSessionRef GetPacketSessionRef() { return static_pointer_cast<PacketSession>(shared_from_this()); }
+
+protected:
+	virtual int32 OnRecv(BYTE* buffer, int32 len) sealed;
+	virtual int32 OnRecvPacket(BYTE* buffer, int32 len) abstract;
+};
+
